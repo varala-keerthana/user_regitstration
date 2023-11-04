b63480f9d55984ddeac29ac0ed285d1b7768f1cb
varala-keerthana <117805518+varala-keerthana@users.noreply.github.com>
2023-11-05 01:29:02 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,168 @@
+*{
+    box-sizing: border-box;
+}
+body{
+    min-height: 100vh;
+    height: 100%;
+    text-align: center;
+    background-color: aquamarine;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-weight: bold;
+    margin: 0;
+      
+}
+ h1{
+            margin: 0;
+            padding: 8px 0;
+ }
+
+label{
+    font-size: 1.4rem;
+}
+.container{
+    text-align: center;
+    width: 40%;
+    height: 470px;
+    padding-top: 40px;
+    margin: 3% 15% 0 5%;
+    background-color: rgb(179, 165, 105);
+    border-radius: 4px;
+    /*opacity: 80%;*/
+}
+.box{
+    display: flex;
+}
+.f1{
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+}
+.f2{
+    display: flex;
+    padding: 20px;
+}
+.c{
+    display: flex;
+    margin-left: 15%;
+}
+.c1{
+    margin: 6px;
+    margin-top: 0;
+}
+.display{
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    height: 500px;
+    width: 30%;
+}
+.div1{
+    display: flex;
+    flex-direction: column;
+    padding-left: 8px;
+}
+.div1 div{
+    padding: 10px;
+}
+.display h2{
+margin-left: 0%;
+color: rgb(52, 57, 61);
+}
+.d{
+  margin-bottom: 20px;
+  width: 100%;
+  height: 210px;
+  background-color: rgb(179, 165, 105);
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  font-family:'Times New Roman', Times, serif;
+}
+.f1 input{
+  
+  border-radius: 5px;
+  height: 30px;
+}
+.f1 input::placeholder{
+    font-weight: bold;
+  color: rgb(209, 181, 181);
+  font-size: 12px;
+}
+.in{
+    width: 70%;
+}
+#img{
+    height: max-content;
+    background-color: rgb(22, 47, 49);
+    font-weight: bold;
+}
+.submit{
+    background-color: rgb(179, 165, 105);
+     font-weight: bold;
+     font-size: 1.2rem;
+     width: 100px;
+     height: 26px;
+     border: 0;
+     border-radius: 4px;
+     box-shadow: 0 0 5px;
+     cursor: pointer;
+}
+
+.submit:active{
+    transform: scale(0.8);
+    box-shadow: 0 0 10px;
+} 
+.img{
+    width: 150px;
+    max-height: 120px;
+    border-radius: 20px;
+    padding: 5px;
+    padding-right: 10px;
+    align-self: center;
+}
+
+@media only screen and(max-width:730px){
+    .box{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .f1{
+        display: flex;
+        flex-direction: column;
+    }
+    .f2{
+        display: flex;
+        flex-direction: column;
+        padding: 23px;
+    }
+    .in{
+        width: 100%;
+    }
+    @media only screen and(max-width:455px){
+        .d{
+            flex-direction: column;
+        }
+        .img{
+            align-self: center;
+        }
+    }
+    .display{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .display h2{
+        margin-left: 0;
+    }
+    .c{
+        display: flex;
+        justify-content: space-around;
+        margin-left: 0;
+    }
+    .submit{
+        margin-bottom: 10px;
+    }
+}
